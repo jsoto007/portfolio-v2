41d88465a1003023f23cd66f2bb10f5666a5aa9e
Jose Soto <jsoto7087@gmail.com>
2023-07-07 19:32:17 -0400

    test02

--- src/app/haeder.js
+++ src/app/haeder.js
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 export default function Haeder() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog as="div" className="lg:hidden">
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
