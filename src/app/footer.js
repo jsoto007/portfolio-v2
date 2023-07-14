72365fe8d88bcfd89a8570023090151ca8f341ff
Jose Soto <jsoto7087@gmail.com>
2023-07-13 22:39:29 -0400

    updated footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -6,10 +6,11 @@ export default function Footer()  {
     <div className="mt-16 bg-blue-950/90 rounded-lg pb-20" id="footer">
     <footer class="footer p-10 bg-neutral text-neutral-content">
   <div className="text-white font-sans text-xl">
-    jsoto7087@gmail.com
+    Email me at: jsoto7087@gmail.com
   </div>
     <a href="https://github.com/jsoto007/supervise-hub-v2/tree/main" target="_black">github</a>
 </footer>
+
     </div>
   )
 }
